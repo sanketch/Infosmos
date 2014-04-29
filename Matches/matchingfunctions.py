b7cdb4c4c489e63f4409a0526280117ba8cf3d9f
joey t <jot240@gmail.com>
2014-04-29 20:14:09 +0800

    creates tables for matching, matching function

---
+++ Matches/matchingfunctions.py
@@ -0,0 +1,27 @@
+'''
+Created on Apr 8, 2014
+
+@author: Mr. Awesomepants
+'''
+from User_Profile.models import UserProfile, Skills, Desires
+from django.contrib.auth.models import User
+from Matches.models import Matches
+def match_user(person):
+    '''This function takes as inpudt user class'''
+    
+    for x in Desires.objects.filter(user__exact = person):
+        r=Matches(user1=person)
+        for y in Skills.objects.filter(skill__exact= x.wants):
+                r.user2 = y.user
+                r.recieving = y.skill
+                r.save
+        
+    
+    
+    
+
+
+
+
+
+
---
+++ Matches/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from User_Profile.models import Skills, Desires, Askill, Adesire,UserProfile
+from django.contrib.auth.models import User
+# Create your models here.
+
+
+
+class Matches(models.Model):
+    user1 =models.ForeignKey(User,related_name='matches_user1')
+    user2 =models.ForeignKey(User,related_name='matches_user2')
+    offering = models.CharField(db_index=True, max_length=40, null=True)
+    recieving =models.CharField(db_index=True, max_length=40, null=True)
+    
+    
+    
+    def __unicode__(self):
+        return self.user1.username
