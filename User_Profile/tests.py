a56d3aecf1be9d01b618b86135fccaa290d45fb8
Amulya <amulya.kattimani@gmail.com>
2014-05-02 02:00:00 +0800

    added tests to User_Profile and Contact

--- User_Profile/tests.py
+++ User_Profile/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
-
+from User_Profile.models import Skills, Desires
 # Create your tests here.
+class SkillsTestCase(TestCase):
+    def setUp(self):
+	    self.skill1 = Skills.objects.create(skill="running", yearxp="10", description="I have run 10 marathons back to back.")
+	
+	def test_skills(self):
+	    runner = Skills.objects.get(skill="running")
+		self.assertEquals(runner.skill,"running")
+		self.skill1.skill = "swimming"
+		
+		self.assertNotEquals(self.skill1.skill,"running")
+		self.assertEquals(self.skill1.skill,"swimming")
+		
+class DesiresTestCase(TestCase):
+    def setUp(self):
+	    self.desires1 = Desires.objects.create(wants="food", description="I am hungry.")
+	
+	def test_desires(self):
+	    foodie = Desires.objects.get(wants="food")
+		self.assertEquals(foodie.wants,"food")
+		self.desires1.wants = "sleep"
+		
+		self.assertNotEquals(self.desires1.wants,"food")
+		self.assertEquals(self.desires1.wants,"sleep")
--- contact/tests.py
+++ contact/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from Contact.models import ContactForm
 
-# Create your tests here.
+class ContactFormTestCase(TestCase):
+    def setUp(self):
+	    self.testingcase = ContactForm.objects.create(name="Bob" email="abcd@a.c" topic="ask you")
+	
+	def test_ContactForm(self):
+	    contact = ContactForm.objects.get(name="Bob")
+		self.assertEquals(contact.name,"Bob")
+		self.testingcase.name = "Husk"
+		
+		self.assertNotEquals(self.testingcase.name,"Bob")
+		self.assertEquals(self.testingcase.name,"Husk")
