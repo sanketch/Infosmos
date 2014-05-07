9693ee4ff1a9da8c2e88a65077025aa7c24bd77a
Woong Gi Chang <cwdzz7@nate.com>
2014-05-08 02:33:31 +0800

    Update models.py

--- User_Profile/models.py
+++ User_Profile/models.py
@@ -59,7 +59,7 @@ class UserProfile(models.Model):
         if len(fb_uid):
             return "http://graph.facebook.com/{}/picture?width=200&height=200".format(fb_uid[0].uid)
 
-        return "/static/img/defaultuser.jpg".format()
+        return "/static/img/defaultuser.png"
 
 
 
